5b8fe4d1efa7e509f2c61a4a2c49b0105d2cf58e
James Andrews <jandrews7348@floridapoly.edu>
2020-03-23 17:49:02 -0400

    insert kernel module
    assumes there are 0 existing video devices

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ echo installing...
 make
 sudo make install
 sudo depmod -a
-sudo modprobe v4l2loopback
+sudo insmod ./v4l2loopback.ko devices=1 video_nr=0 exclusive_caps=1
+#sudo modprobe v4l2loopback
